168d58cefa6b93473498c0774919998fae325cc5
Alexander Osadchy <a.i.osadchy@gmail.com>
2022-02-23 04:54:03 +0300

    Update font

--- simple-text-only-theme/start.sh
+++ simple-text-only-theme/start.sh
@@ -4,15 +4,10 @@ readonly SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 cd "${SCRIPT_DIR}"
 
-sleep 20
+sleep 10
 conky -c "./memory.conkyrc"     &
-sleep 1
 conky -c "./cpu.conkyrc"        &
-sleep 1
 conky -c "./network.conkyrc"    &
-sleep 1
 conky -c "./disks.conkyrc"      &
-sleep 1
 conky -c "./clock.conkyrc"      &
-sleep 1
 conky -c "./weather.conkyrc"    &
