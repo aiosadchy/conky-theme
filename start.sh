91b1e5fa6c05b1152837b0544c0b1ab7106c0ad4
Alexander Osadchy <a.i.osadchy@gmail.com>
2022-04-06 00:50:42 +0300

    improve configuratoion variable parsing

--- start.sh
+++ start.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
 readonly SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-readonly SLEEP_TIME=${1:-"10"}
+readonly SLEEP_TIME="${1:-"10"}"
 
 cd "${SCRIPT_DIR}"
 
 sleep "${SLEEP_TIME}"
-env $(cat configuration.env | xargs) conky -c "./memory.conkyrc"     &
-env $(cat configuration.env | xargs) conky -c "./cpu.conkyrc"        &
-env $(cat configuration.env | xargs) conky -c "./network.conkyrc"    &
-env $(cat configuration.env | xargs) conky -c "./disks.conkyrc"      &
-env $(cat configuration.env | xargs) conky -c "./clock.conkyrc"      &
-env $(cat configuration.env | xargs) conky -c "./weather.conkyrc"    &
+
+set -a
+. configuration.sh
+set +a
+
+conky --daemonize --quiet -c "./memory.conkyrc"
+conky --daemonize --quiet -c "./cpu.conkyrc"
+conky --daemonize --quiet -c "./network.conkyrc"
+conky --daemonize --quiet -c "./disks.conkyrc"
+conky --daemonize --quiet -c "./clock.conkyrc"
+conky --daemonize --quiet -c "./weather.conkyrc"
